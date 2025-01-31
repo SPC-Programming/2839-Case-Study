1898c93911b2ae6627c7ef0ca527fc4959389b10
James Vernon <Vernon.James@SPCollege.edu>
2025-01-31 14:48:00 -0500

    commit week3

--- SportsPro/Controllers/ProductController.cs
+++ SportsPro/Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using SportsPro.Models;
 
@@ -18,6 +19,25 @@ namespace SportsPro.Controllers
             ProductsList plm = new ProductsList();
             plm.ProductList = _context.Products.ToList<Product>();
             return View(plm);
+        }
+
+        // [HttpPost]
+        // public IActionResult Edit(int productId)
+        // {
+        //     Product product = _context.Products.Find(productId)!;
+        //     return View("ProductEdit", product);
+        // }
+        // [HttpGet]
+        // public IActionResult ProductEdit(Product product)
+        // {
+        //     return View(product);
+        // }
+        public IActionResult Details(int? productId, decimal? price){
+            var products = _context.Products.Where(p => p.YearlyPrice >= price);//linq
+            ProductsList productsByPrice = new ProductsList();
+            productsByPrice.ProductList = (List<Product>)products; 
+
+            return View("List", productsByPrice);
         }
 
 
